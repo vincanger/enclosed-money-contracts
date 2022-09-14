c6391249f47d1fcba82b88f5f6d2715487a96205
vincanger <70215737+vincanger@users.noreply.github.com>
2022-09-14 09:32:08 +0200

    deploy bill example contract

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -1,12 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
+import 'forge-std/Script.sol';
+import 'src/BillExample.sol';
 
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        
+        BillExample bill = new BillExample();
+        bill.example();
+        bill.example1();
+        vm.stopBroadcast();
     }
 }
